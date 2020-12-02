6fbb00e29dc5e942f6873d669278658d90037c3d
EvelynHdez <evs.hdezag@gmail.com>
2020-12-02 01:53:23 -0600

    tarea 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { CookieService } from 'ngx-cookie-service';
+import {JsonService} from './json.service';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +8,46 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
+  title = 'http-client';
+
+  http;
+  constructor (
+    private JsonService : JsonService,
+    private cookie: CookieService
+    ){
   }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+  ngOnInit(){
   }
+
+getJson(cityName: string){
+    this.JsonService.getJson(cityName)
+    .subscribe(
+      res =>{
+        console.log(res);
+        this.cookie.set("ciudad",res["name"]);
+        this.cookie.set("vientos",res["wind"].deg);
+
+        alert("Los vientos de " + this.cookie.get("ciudad") + " son de " + this.cookie.get("viento") + " km por hora");
+
+        this.http = res},
+      err => console.log(err)
+    )
+
+  }
+
+  submitLocation(cityName: HTMLInputElement,countryCode: HTMLInputElement){
+    if(cityName.value){
+      this.getJson(cityName.value);
+      cityName.value = '';
+    }
+    else{
+      alert('Inserta algun dato')
+    }
+    cityName.focus();
+    return false;
+
+  }
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,13 @@
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
+import { CookieService } from 'ngx-cookie-service';
 @NgModule({
   declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  imports: [BrowserModule, HttpClientModule],
+  providers: [ 
+    CookieService
   ],
   bootstrap: [AppComponent]
 })
